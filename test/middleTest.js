2957b7c3a912eef2298a0791826c17f6ee4d72fe
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-01 20:22:43 -0600

    DiogoCostaAle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,15 @@
+const chai = require('chai');
+const deepEqual = chai.assert.deepEqual;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require("../middle");
 
 
+describe("#middle", () => {
+  it("should return [3] of [1,2,3,4,5]", () => {
+    deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-
+  it("should return [3,4] of [1,2,3,4,5,6]", () => {
+    deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
