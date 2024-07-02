351f2d942cda0242f7b4a6a1fb9b70c6c470bd22
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-01 20:19:47 -0600

    aiaiai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,27 @@
+const assert = require('chai').assert;
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
 
-const singleElementArray = [1];
-const tailSingleElementArray = tail(singleElementArray);
-assertEqual(tailSingleElementArray.length, 0);
-assertEqual(singleElementArray.length, 1);
+describe("#tail", () => {
+  it("should return an array with the elements excluding the first one", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
+  });
 
-const emptyArray = [];
-const tailEmptyArray = tail(emptyArray);
-assertEqual(tailEmptyArray.length, 0);
-assertEqual(emptyArray.length, 0);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words)[1], "Labs");
+  it("should return an empty array when passed an array with a single element", () => {
+    const singleElementArray = [1];
+    const result = tail(singleElementArray);
+    assert.strictEqual(result.length, 0);
+  });
+
+  it("should return an empty array when passed an empty array", () => {
+    const emptyArray = [];
+    const result = tail(emptyArray);
+    assert.strictEqual(result.length, 0);
+  });
+
+});
